335f0566fcdc2b3765c7657f79aed52642e0d37d
sungchul09 <sungchul09@naver.com>
2021-06-03 01:01:25 +0900

    화살표 폴더(충돌폴더) 해결

---
+++ vue/vue-todo/src/components/TodoFooter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearTodo">Clear All </span>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clearTodo: function() {
+      localStorage.clear();
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f6;
+}
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllBtn {
+  color: #e20303;
+  display: block;
+}
+</style>
